470a950b2c9b3dc21f655ee560c80ebfec0edaed
mcjczapiewski <59090975+mcjczapiewski@users.noreply.github.com>
2020-07-03 13:51:02 +0200

    Add interface

---
+++ IVehicleDao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace daoHomework
+{
+    public interface IVehicleDao
+    {
+        public List<Vehicle> GetAllVehicles();
+        public Vehicle GetVehicle(int vehicleID);
+        public void UpdateVehicle(Vehicle vehicle);
+        public void DeleteVehicle(Vehicle vehicle);
+    }
+}
--- Vehicle.cs
+++ Vehicle.cs
@@ -2,8 +2,17 @@ namespace daoHomework
 {
     public class Vehicle
     {
+        public int VehicleID { get; set; }
         public string VehicleType { get; set; }
         public int WheelsNumber { get; set; }
         public int MaxPassengersNumber { get; set; }
+
+        public Vehicle(int vehicleID, string vehicleType, int wheelsNumber, int maxPassengersNumber)
+        {
+            VehicleID = vehicleID;
+            VehicleType = vehicleType;
+            WheelsNumber = wheelsNumber;
+            MaxPassengersNumber = maxPassengersNumber;
+        }
     }
 }
