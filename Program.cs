399bfddc6cdb5cb910e04c306363fa4f890b4426
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-07-12 15:40:19 +0200

    Address issue #4, move Program content to methods in ProgramLogic

--- Program.cs
+++ Program.cs
@@ -8,48 +8,13 @@ namespace daoHomework
         {
             IVehicleDao vehicleDao = new VehicleDaoImpl();
 
-            //print all
-            foreach (var vehicle in vehicleDao.GetAllVehicles())
-            {
-                Console.WriteLine($"Vehicle ID: {vehicle.VehicleID}, " +
-                    $"Type: {vehicle.VehicleType}, " +
-                    $"Wheels: {vehicle.WheelsNumber}, " +
-                    $"Passengers capacity: {vehicle.MaxPassengersNumber}");
-            }
+            ProgramLogic.PrintAllVehicles(vehicleDao);
 
-            //update
-            Console.WriteLine("What is the Vehicle ID to update?");
-            Console.Write("> ");
-            var passedValue = Console.ReadLine();
-            var userPassedID = Convert.ToInt32(passedValue);
-            var vehicleToUpdate = new Vehicle(userPassedID);
+            ProgramLogic.UpdateValue(vehicleDao);
 
-            Console.WriteLine("What should be new available seats value?");
-            Console.Write("> ");
-            passedValue = Console.ReadLine();
-            int availableSeats = Convert.ToInt32(passedValue);
-            vehicleToUpdate.MaxPassengersNumber = availableSeats;
+            ProgramLogic.GetSpecificRecord(vehicleDao);
 
-            vehicleDao.UpdateMaxPassengersNumber(vehicleToUpdate);
-
-            //get Vehicle
-            Console.WriteLine("What is the Vehicle ID you want to retrieve information?");
-            Console.Write("> ");
-            passedValue = Console.ReadLine();
-            userPassedID = Convert.ToInt32(passedValue);
-            var vehicleRetrieved = vehicleDao.GetVehicle(userPassedID);
-            Console.WriteLine($"Vehicle ID: {vehicleRetrieved.VehicleID}, " +
-                $"Type: {vehicleRetrieved.VehicleType}, " +
-                $"Wheels: {vehicleRetrieved.WheelsNumber}, " +
-                $"Passengers capacity: {vehicleRetrieved.MaxPassengersNumber}");
-
-            //delete
-            Console.WriteLine("What is the Vehicle ID to delete?");
-            Console.Write("> ");
-            passedValue = Console.ReadLine();
-            userPassedID = Convert.ToInt32(passedValue);
-            var vehicleToDelete = vehicleDao.GetVehicle(userPassedID);
-            vehicleDao.DeleteVehicle(vehicleToDelete);
+            ProgramLogic.DeleteRecord(vehicleDao);
         }
     }
 }
---
+++ ProgramLogic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace daoHomework
+{
+    internal class ProgramLogic
+    {
+        internal static void DeleteRecord(IVehicleDao vehicleDao)
+        {
+            GetUserInput("What is the Vehicle ID to delete?",
+                         out int userPassedValue);
+            var vehicleToDelete = vehicleDao.GetVehicle(userPassedValue);
+            vehicleDao.DeleteVehicle(vehicleToDelete);
+        }
+
+        internal static void GetSpecificRecord(IVehicleDao vehicleDao)
+        {
+            GetUserInput("What is the Vehicle ID you want to retrieve information?",
+                         out int userPassedValue);
+            var vehicleRetrieved = vehicleDao.GetVehicle(userPassedValue);
+            Console.WriteLine($"Vehicle ID: {vehicleRetrieved.VehicleID}, " +
+                $"Type: {vehicleRetrieved.VehicleType}, " +
+                $"Wheels: {vehicleRetrieved.WheelsNumber}, " +
+                $"Passengers capacity: {vehicleRetrieved.MaxPassengersNumber}");
+        }
+
+        internal static void PrintAllVehicles(IVehicleDao vehicleDao)
+        {
+            foreach (var vehicle in vehicleDao.GetAllVehicles())
+            {
+                Console.WriteLine($"Vehicle ID: {vehicle.VehicleID}, " +
+                    $"Type: {vehicle.VehicleType}, " +
+                    $"Wheels: {vehicle.WheelsNumber}, " +
+                    $"Passengers capacity: {vehicle.MaxPassengersNumber}");
+            }
+        }
+
+        internal static void UpdateValue(IVehicleDao vehicleDao)
+        {
+            GetUserInput("What is the Vehicle ID to update?",
+                         out int userPassedValue);
+            var vehicleToUpdate = new Vehicle(userPassedValue);
+
+            GetUserInput("What should be new available seats value?",
+                         out userPassedValue);
+            vehicleToUpdate.MaxPassengersNumber = userPassedValue;
+
+            vehicleDao.UpdateMaxPassengersNumber(vehicleToUpdate);
+        }
+
+        private static void GetUserInput(string message, out int userPassedValue)
+        {
+            Console.WriteLine(message);
+            Console.Write("> ");
+            var passedValue = Console.ReadLine();
+            userPassedValue = Convert.ToInt32(passedValue);
+        }
+    }
+}
